7bd403ed5e0b5848fee211546ac09115f7a68d77
Holger Woltersdorf <hw@hollo.me>
2018-01-02 00:51:32 +0100

    Adds dead letter message implementation

---
+++ src/Messages/DeadLetter.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+/**
+ * @author hollodotme
+ */
+
+namespace PHPMQ\Protocol\Messages;
+
+use PHPMQ\Protocol\Constants\PacketType;
+use PHPMQ\Protocol\Constants\ProtocolVersion;
+use PHPMQ\Protocol\Interfaces\IdentifiesMessage;
+use PHPMQ\Protocol\Interfaces\IdentifiesMessageType;
+use PHPMQ\Protocol\Interfaces\IdentifiesQueue;
+use PHPMQ\Protocol\Interfaces\ProvidesMessageData;
+use PHPMQ\Protocol\Traits\StringRepresenting;
+use PHPMQ\Protocol\Types\MessageHeader;
+use PHPMQ\Protocol\Types\MessageType;
+use PHPMQ\Protocol\Types\PacketHeader;
+
+/**
+ * Class DeadLetter
+ * @package PHPMQ\Protocol\Messages
+ */
+final class DeadLetter implements ProvidesMessageData
+{
+	use StringRepresenting;
+
+	/** @var IdentifiesQueue */
+	private $queueName;
+
+	/** @var IdentifiesMessage */
+	private $messageId;
+
+	/** @var IdentifiesMessageType */
+	private $messageType;
+
+	public function __construct( IdentifiesQueue $queueName, IdentifiesMessage $messageId )
+	{
+		$this->queueName   = $queueName;
+		$this->messageId   = $messageId;
+		$this->messageType = new MessageType( MessageType::DEAD_LETTER );
+	}
+
+	public function getMessageType() : IdentifiesMessageType
+	{
+		return $this->messageType;
+	}
+
+	public function getQueueName() : IdentifiesQueue
+	{
+		return $this->queueName;
+	}
+
+	public function getMessageId() : IdentifiesMessage
+	{
+		return $this->messageId;
+	}
+
+	public function toString() : string
+	{
+		$messageHeader         = new MessageHeader( ProtocolVersion::VERSION_1, $this->messageType );
+		$queuePacketHeader     = new PacketHeader( PacketType::QUEUE_NAME, \strlen( $this->queueName->toString() ) );
+		$messageIdPacketHeader = new PacketHeader( PacketType::MESSAGE_ID, \strlen( $this->messageId->toString() ) );
+
+		return $messageHeader->toString()
+			   . $queuePacketHeader->toString()
+			   . $this->queueName
+			   . $messageIdPacketHeader->toString()
+			   . $this->messageId;
+	}
+}
